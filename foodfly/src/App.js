89390f3bc4a43d1cdc9f1fd27c45cb292c636934
Anisa <anisaemin97@gmail.com>
2023-03-19 00:20:30 +0200

    added server

--- foodfly/src/App.js
+++ foodfly/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { Catalog } from './components/Catalog';
-import { Home } from './components/Home';
-import { NavigationBar } from './components/navigationBar';
+import { Home } from './components/Home/Home';
+import { NavigationBar } from './components/NavigationBar/navigationBar';
 import {Footer} from './components/Footer'
-import { Register } from './components/Register';
+import { Register } from './components/NavigationBar/Register';
 import {Detail} from './components/Detail'
 import {Error} from './components/Error'
-import { Login } from './components/Login';
+import { Login } from './components/NavigationBar/Login';
 import {CreateRecipe} from './components/CreateRecipe'
 
 function App() {
